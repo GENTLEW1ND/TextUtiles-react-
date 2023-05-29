dbe5fed1165e4ddc19166f84ea9650cc07760e1a
GENTLEW1ND <rajchakraborty029@gmail.com>
2023-05-29 18:34:41 +0530

    states

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
     {/* <Navbar title = "TextUtiles2" aboutText="about us"/> this are called props */}
-    <Navbar/> 
+    <Navbar title='TextUtils'/> 
+    <div className='container my-3'>
+    <TextForm heading="Enter your text to analize below"/>
+    </div>
     </>
   );
 }
